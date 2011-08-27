38ca2b87e9e79fc67e1f283dada40498720bc093
fschwiet <fschwiet@gmail.com>
2011-08-27 13:47:03 -0700

    adding build task

--- default.ps1
+++ default.ps1
@@ -1,5 +1,19 @@
+properties {
+    $baseDirectory  = resolve-path .
+    $buildDirectory = ($buildDirectory, "$baseDirectory\build") | select -first 1
+}
 
-task default -depends Run
+task default -depends Build
 
-task Run {
+task Cleanup {
+    if (test-path $buildDirectory) {
+        rm $buildDirectory -recurse
+    }
+}
+
+task Build {
+    $v4_net_version = (ls "$env:windir\Microsoft.NET\Framework\v4.0*").Name
+    $dearlySolution = "$baseDirectory\dearly.sln"
+
+    exec { &"C:\Windows\Microsoft.NET\Framework\$v4_net_version\MSBuild.exe" ManyConsole.sln /T:"Clean,Build" /property:OutDir="$buildDirectory\"` }    
 }
