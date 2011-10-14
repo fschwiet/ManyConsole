2ba8576c180a8138b78053730c75832640f71694
fschwiet <fschwiet@gmail.com>
2011-10-14 00:04:10 -0700

    updating version

--- default.ps1
+++ default.ps1
@@ -1,7 +1,7 @@
 properties {
     $baseDirectory  = resolve-path .
     $buildDirectory = ($buildDirectory, "$baseDirectory\build") | select -first 1
-    $version = "0.3.4"
+    $version = "0.3.5"
 
     $shortDescription = "A library for writing console applications.  Extends NDesk.Options to support separate commands from one console application."
 }
