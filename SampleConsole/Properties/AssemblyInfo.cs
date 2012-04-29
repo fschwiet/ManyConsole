8b323031227191b24ba054eab8bfac131cb5be35
fschwiet <fschwiet@gmail.com>
2012-04-29 01:31:26 -0700

    updating version

--- SampleConsole/Properties/AssemblyInfo.cs
+++ SampleConsole/Properties/AssemblyInfo.cs
@@ -8,13 +8,13 @@ using System.Runtime.InteropServices;
 #endif
 [assembly: CLSCompliantAttribute(false )]
 [assembly: ComVisibleAttribute(false)]
-[assembly: AssemblyTitleAttribute("SampleConsole 0.4.2.5")]
+[assembly: AssemblyTitleAttribute("SampleConsole 0.4.2.6")]
 [assembly: AssemblyDescriptionAttribute("A library for writing console applications.  Extends NDesk.Options to support separate commands from one console application.")]
 [assembly: AssemblyCompanyAttribute("n/a")]
-[assembly: AssemblyProductAttribute("ManyConsole 0.4.2.5")]
+[assembly: AssemblyProductAttribute("ManyConsole 0.4.2.6")]
 [assembly: AssemblyCopyrightAttribute("Copyright © Frank Schwieterman 2011")]
-[assembly: AssemblyVersionAttribute("0.4.2.5")]
-[assembly: AssemblyInformationalVersionAttribute("0.4.2.5 / 0000000")]
-[assembly: AssemblyFileVersionAttribute("0.4.2.5")]
+[assembly: AssemblyVersionAttribute("0.4.2.6")]
+[assembly: AssemblyInformationalVersionAttribute("0.4.2.6 / 0000000")]
+[assembly: AssemblyFileVersionAttribute("0.4.2.6")]
 [assembly: AssemblyDelaySignAttribute(false)]
 
