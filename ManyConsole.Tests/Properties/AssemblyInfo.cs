43808695229ed229a700133da67ec3bfcbd686ec
fschwiet <fschwiet@gmail.com>
2012-06-22 16:16:22 -0700

    updating version

--- ManyConsole.Tests/Properties/AssemblyInfo.cs
+++ ManyConsole.Tests/Properties/AssemblyInfo.cs
@@ -8,13 +8,13 @@ using System.Runtime.InteropServices;
 #endif
 [assembly: CLSCompliantAttribute(false )]
 [assembly: ComVisibleAttribute(false)]
-[assembly: AssemblyTitleAttribute("ManyConsole.Tests 0.4.2.7")]
+[assembly: AssemblyTitleAttribute("ManyConsole.Tests 0.4.2.8")]
 [assembly: AssemblyDescriptionAttribute("A library for writing console applications.  Extends NDesk.Options to support separate commands from one console application.")]
 [assembly: AssemblyCompanyAttribute("n/a")]
-[assembly: AssemblyProductAttribute("ManyConsole 0.4.2.7")]
+[assembly: AssemblyProductAttribute("ManyConsole 0.4.2.8")]
 [assembly: AssemblyCopyrightAttribute("Copyright © Frank Schwieterman 2011")]
-[assembly: AssemblyVersionAttribute("0.4.2.7")]
-[assembly: AssemblyInformationalVersionAttribute("0.4.2.7 / 0000000")]
-[assembly: AssemblyFileVersionAttribute("0.4.2.7")]
+[assembly: AssemblyVersionAttribute("0.4.2.8")]
+[assembly: AssemblyInformationalVersionAttribute("0.4.2.8 / 0000000")]
+[assembly: AssemblyFileVersionAttribute("0.4.2.8")]
 [assembly: AssemblyDelaySignAttribute(false)]
 
