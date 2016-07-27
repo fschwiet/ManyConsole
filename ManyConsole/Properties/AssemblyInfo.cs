a94f163419207e6f30bb12ba42e050739b12ba01
fschwiet <fschwiet@gmail.com>
2016-07-27 13:59:09 -0500

    update version number

--- ManyConsole/Properties/AssemblyInfo.cs
+++ ManyConsole/Properties/AssemblyInfo.cs
@@ -8,13 +8,13 @@ using System.Runtime.InteropServices;
 #endif
 [assembly: CLSCompliantAttribute(false )]
 [assembly: ComVisibleAttribute(false)]
-[assembly: AssemblyTitleAttribute("ManyConsole 0.4.2.19")]
+[assembly: AssemblyTitleAttribute("ManyConsole 0.4.2.20")]
 [assembly: AssemblyDescriptionAttribute("A library for writing console applications.  Extends NDesk.Options to support separate commands from one console application.")]
 [assembly: AssemblyCompanyAttribute("n/a")]
-[assembly: AssemblyProductAttribute("ManyConsole 0.4.2.19")]
+[assembly: AssemblyProductAttribute("ManyConsole 0.4.2.20")]
 [assembly: AssemblyCopyrightAttribute("Copyright © Frank Schwieterman 2011")]
-[assembly: AssemblyVersionAttribute("0.4.2.19")]
-[assembly: AssemblyInformationalVersionAttribute("0.4.2.19 / 008361a")]
-[assembly: AssemblyFileVersionAttribute("0.4.2.19")]
+[assembly: AssemblyVersionAttribute("0.4.2.20")]
+[assembly: AssemblyInformationalVersionAttribute("0.4.2.20 / cbc5a60")]
+[assembly: AssemblyFileVersionAttribute("0.4.2.20")]
 [assembly: AssemblyDelaySignAttribute(false)]
 
