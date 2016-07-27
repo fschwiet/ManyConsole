084d47c57b6aaa5c4602ad647658169a56f7457f
fschwiet <fschwiet@gmail.com>
2016-07-27 13:55:10 -0500

    updating version (checkin date is misleading/late)

--- ManyConsole/Properties/AssemblyInfo.cs
+++ ManyConsole/Properties/AssemblyInfo.cs
@@ -3,15 +3,18 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: CLSCompliant(false )]
-[assembly: ComVisible(false)]
-[assembly: AssemblyTitle("ManyConsole 0.4.2.17")]
-[assembly: AssemblyDescription("A library for writing console applications.  Extends NDesk.Options to support separate commands from one console application.")]
-[assembly: AssemblyCompany("n/a")]
-[assembly: AssemblyProduct("ManyConsole 0.4.2.17")]
-[assembly: AssemblyCopyright("Copyright © Frank Schwieterman 2011")]
-[assembly: AssemblyVersion("0.4.2.17")]
-[assembly: AssemblyInformationalVersion("0.4.2.17 / c15e683")]
-[assembly: AssemblyFileVersion("0.4.2.17")]
-[assembly: AssemblyDelaySign(false)]
+#if !SILVERLIGHT
+[assembly: SuppressIldasmAttribute()]
+#endif
+[assembly: CLSCompliantAttribute(false )]
+[assembly: ComVisibleAttribute(false)]
+[assembly: AssemblyTitleAttribute("ManyConsole 0.4.2.19")]
+[assembly: AssemblyDescriptionAttribute("A library for writing console applications.  Extends NDesk.Options to support separate commands from one console application.")]
+[assembly: AssemblyCompanyAttribute("n/a")]
+[assembly: AssemblyProductAttribute("ManyConsole 0.4.2.19")]
+[assembly: AssemblyCopyrightAttribute("Copyright © Frank Schwieterman 2011")]
+[assembly: AssemblyVersionAttribute("0.4.2.19")]
+[assembly: AssemblyInformationalVersionAttribute("0.4.2.19 / 008361a")]
+[assembly: AssemblyFileVersionAttribute("0.4.2.19")]
+[assembly: AssemblyDelaySignAttribute(false)]
 
--- SampleConsole/Properties/AssemblyInfo.cs
+++ SampleConsole/Properties/AssemblyInfo.cs
@@ -8,13 +8,13 @@ using System.Runtime.InteropServices;
 #endif
 [assembly: CLSCompliantAttribute(false )]
 [assembly: ComVisibleAttribute(false)]
-[assembly: AssemblyTitleAttribute("SampleConsole 0.4.2.17")]
+[assembly: AssemblyTitleAttribute("SampleConsole 0.4.2.19")]
 [assembly: AssemblyDescriptionAttribute("A library for writing console applications.  Extends NDesk.Options to support separate commands from one console application.")]
 [assembly: AssemblyCompanyAttribute("n/a")]
-[assembly: AssemblyProductAttribute("ManyConsole 0.4.2.17")]
+[assembly: AssemblyProductAttribute("ManyConsole 0.4.2.19")]
 [assembly: AssemblyCopyrightAttribute("Copyright © Frank Schwieterman 2011")]
-[assembly: AssemblyVersionAttribute("0.4.2.17")]
-[assembly: AssemblyInformationalVersionAttribute("0.4.2.17 / c15e683")]
-[assembly: AssemblyFileVersionAttribute("0.4.2.17")]
+[assembly: AssemblyVersionAttribute("0.4.2.19")]
+[assembly: AssemblyInformationalVersionAttribute("0.4.2.19 / 008361a")]
+[assembly: AssemblyFileVersionAttribute("0.4.2.19")]
 [assembly: AssemblyDelaySignAttribute(false)]
 
