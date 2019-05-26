7811c5ee88c1bf7bb35e328a2496e6b458b5704b
Søren Bech Christensen <sbc@bangding.dk>
2019-05-26 09:58:44 +0200

    Adapted to netstandard2.0

--- ManyConsole/Internal/ConsoleRedirectionDetection.cs
+++ ManyConsole/Internal/ConsoleRedirectionDetection.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace ManyConsole.Internal
+﻿namespace ManyConsole.Internal
 {
     using System;
-    using System.Runtime.InteropServices;
 
     // implementation from http://stackoverflow.com/questions/3453220/how-to-detect-if-console-in-stdin-has-been-redirected
 
@@ -21,23 +15,16 @@ namespace ManyConsole.Internal
     {
         public bool IsOutputRedirected()
         {
-            return FileType.Char != GetFileType(GetStdHandle(StdHandle.Stdout));
+            return Console.IsOutputRedirected;
         }
         public bool IsInputRedirected()
         {
-            return FileType.Char != GetFileType(GetStdHandle(StdHandle.Stdin));
+            return Console.IsInputRedirected;
         }
         public bool IsErrorRedirected()
         {
-            return FileType.Char != GetFileType(GetStdHandle(StdHandle.Stderr));
+            return Console.IsErrorRedirected;
         }
 
-        // P/Invoke:
-        private enum FileType { Unknown, Disk, Char, Pipe };
-        private enum StdHandle { Stdin = -10, Stdout = -11, Stderr = -12 };
-        [DllImport("kernel32.dll")]
-        private static extern FileType GetFileType(IntPtr hdl);
-        [DllImport("kernel32.dll")]
-        private static extern IntPtr GetStdHandle(StdHandle std);
     }
 }
