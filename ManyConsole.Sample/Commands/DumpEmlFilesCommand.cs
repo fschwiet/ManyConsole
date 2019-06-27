7c829b68b1f9dd167c42b911fc0565a9f8b9f618
Mohsen Seifi <mohsens2@outlook.com>
2019-06-27 17:56:57 +0430

    Ported console sample

---
+++ ManyConsole.Sample/Commands/DumpEmlFilesCommand.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using ManyConsole;
+using MimeKit;
+
+namespace SampleConsole
+{
+    public class DumpEmlFilesCommand : ConsoleCommand
+    {
+        public DumpEmlFilesCommand()
+        {
+            this.IsCommand("dump-eml", "Prints the contents of eml file(s).");
+
+            this.HasOption("r|recursive", "Print files recursively", v => Recursive = v != null);
+            this.HasOption("h|header=", "Mail header to include", v => HeadersToPrint.Add(v));
+
+            HasAdditionalArguments(1, "<fileOrDirectory>");
+        }
+
+        public string Path;
+        public bool Recursive;
+        public List<string> HeadersToPrint = new List<string>();
+
+        public override int Run(string[] remainingArguments)
+        {
+            Path = remainingArguments[0];
+
+            if (File.Exists(Path))
+            {
+                PrintEmlFile(Path);
+            }
+            else if (Directory.Exists(Path))
+            {
+                PrintEmlDirectory(Path);
+            }
+            else
+            {
+                throw new Exception("Could not find file or directory at " + Path);
+            }
+
+            return 0;
+        }
+
+        private void PrintEmlDirectory(string directory)
+        {
+            var di = new DirectoryInfo(directory);
+
+            foreach(var entry in di.GetFileSystemInfos())
+            {
+                if (entry is FileInfo)
+                {
+                    PrintEmlFile(entry.FullName);
+                }
+                else if (Recursive && entry is DirectoryInfo)
+                {
+                    PrintEmlDirectory(entry.FullName);
+                }
+            }
+        }
+
+        private void PrintEmlFile(string filepath)
+        {
+            var mail = MimeMessage.Load(filepath);
+
+            Console.WriteLine("SUBJECT:\t{0}", mail.Subject);
+            Console.WriteLine("FROM:\t{0}", mail.From);
+
+            foreach(var to in mail.To)
+            {
+                Console.WriteLine("TO:\t{0}", to);
+            }
+            
+            var headersPresent = mail.Headers;
+
+            foreach (var header in HeadersToPrint)
+            {
+                if (headersPresent.Contains(header.ToLower()))
+                {
+                    Console.WriteLine("{0}:\t{1}", header.ToUpper(), mail.Headers[header]);
+                }
+            }
+
+            Console.WriteLine("BODY:");
+            Console.WriteLine(mail.Body);
+        }
+    }
+}
---
+++ ManyConsole.Sample/Commands/ExampleCommand.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using ManyConsole;
+using Mono.Options;
+
+namespace MC.AX.DataUtility
+{
+    /// <summary>
+    /// Example implementation of a ManyConsole command-line argument parser (ConsoleCommand) class
+    /// </summary>
+    public class ExampleCommand : ConsoleCommand
+    {
+        /// <summary>
+        /// Configure command options and describe details in the class contructor
+        /// </summary>
+        /// <example>
+        /// Example usage:
+        /// 
+        /// SampleConsole.exe example "option one" two -b -o "optional argument" -l=first -l=second -l "the third option"
+        /// 
+        /// Expected output:
+        /// 
+        /// Executing Example (Example implementation of a ManyConsole command-line argument
+        /// parser Command):
+        ///    Argument1 : option one
+        ///    Argument2 : two
+        ///    BooleanOption : True
+        ///    OptionalArgument1 : optional argument
+        ///    OptionalArgumentList : System.Collections.Generic.List`1[System.String]
+        ///
+        ///Called Example command - Argument1 = "option one" Argument2 = "two" BooleanOptio
+        ///n: True
+        ///List Item 0 = "first"
+        ///List Item 1 = "second"
+        ///List Item 2 = "the third option"
+        /// </example>
+        public ExampleCommand()
+        {
+            this.IsCommand("Example", "Example implementation of a ManyConsole command-line argument parser Command");
+
+            this.HasOption("b|booleanOption", "Boolean flag option", b => BooleanOption = true);
+
+            //  Setting .Options directly is the old way to do this, you may prefer to call the helper
+            //  method HasOption/HasRequiredOption.
+            Options = new OptionSet()
+            {
+                {"l|list=", "Values to add to list", v => OptionalArgumentList.Add(v)},
+                {"r|requiredArguments=", "Optional string argument requiring a value be specified afterwards", s => OptionalArgument1 = s},
+                {"o|optionalArgument:", "Optional String argument which is null if no value follow is specified", s => OptionalArgument2 = s ?? "<no argument specified>"}
+            };
+
+            this.HasRequiredOption("requiredOption=", "Required string argument also requiring a value.", s => { });
+            this.HasOption("anotherOptional=", "Another way to specify optional arguments", s => {});
+
+            HasAdditionalArguments(2, "<Argument1> <Argument2>");
+        }
+
+        public string Argument1;
+        public string Argument2;
+        public string OptionalArgument1;
+        public string OptionalArgument2;
+        public bool BooleanOption;
+        public List<string> OptionalArgumentList = new List<string>();
+
+        public override int Run(string[] remainingArguments)
+        {
+            Argument1 = remainingArguments[0];
+            Argument2 = remainingArguments[1];
+
+            Console.WriteLine(@"Called Example command - Argument1 = ""{0}"" Argument2 = ""{1}"" BooleanOption: {2}", Argument1, Argument2, BooleanOption);
+
+            OptionalArgumentList.ForEach((item) => Console.WriteLine(@"List Item {0} = ""{1}""", OptionalArgumentList.IndexOf(item), item));
+
+            if (BooleanOption)
+            {
+                throw new Exception("Throwing unhandled exception because BooleanOption is true");
+            }
+
+            return 0;
+        }
+    }
+}
